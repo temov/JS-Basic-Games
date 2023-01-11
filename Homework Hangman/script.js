79847d1dab272d745d0483572c9f167dedef238f
Tode Temov <75773817+temov@users.noreply.github.com>
2023-01-11 16:32:50 +0100

    Add files via upload

---
+++ Homework Hangman/script.js
@@ -0,0 +1,22 @@
+// add letters
+
+
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+window.onload = function(){
+    let letter = document.getElementById('letters');
+    letter.innerHTML = `<ul>`;
+
+    for(let i=0; i<alphabet.length; i++){
+
+        letter.innerHTML+=`<li>${alphabet[i]}</li>`;
+    }
+
+    letter.innerHTML = `</ul>`;
+    console.log(letter);
+        
+}
